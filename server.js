cacc64a01e6549aa414d9fca22778b53efdf64a7
Johan Magnusson <joma2304@student.miun.se>
2024-05-04 14:49:56 +0200

    Lagt till cors rätt

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
-app.use(cors);
+app.use(cors());
 
 //Routes
 app.use("/api", authRoutes);
