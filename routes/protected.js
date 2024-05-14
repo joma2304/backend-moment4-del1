5b10f951e8197f69b026f827f5a5d2cc69eb6285
Johan Magnusson <joma2304@student.miun.se>
2024-05-14 21:02:40 +0200

    Fixat GET username

--- routes/protected.js
+++ routes/protected.js
@@ -5,24 +5,31 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-
 router.get('/username', async (req, res) => {
   try {
-    // Använd användarnamnet från användaren
-    const loggedInUsername = req.user.username;
+    // Hämta användarnamnet från JWT-tokenet
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid JWT token' });
+      }
+      const loggedInUsername = decoded.username;
 
-    // Hämta den inloggade användaren baserat på användarnamnet
-    const loggedInUser = await User.findOne({ username: loggedInUsername });
+      // Hämta den inloggade användaren baserat på användarnamnet
+      const loggedInUser = await User.findOne({ username: loggedInUsername });
 
-    // Om den inloggade användaren inte hittas
-    if (!loggedInUser) {
-      return res.status(404).json({ message: 'Inloggad användare hittades inte.' });
-    }
+      // Om den inloggade användaren inte hittas
+      if (!loggedInUser) {
+        return res.status(404).json({ message: 'Inloggad användare hittades inte.' });
+      }
 
-    // Returnera den inloggade användaren
-    return res.json(loggedInUser);
+      // Returnera den inloggade användaren
+      return res.json(loggedInUser);
+    });
   } catch (error) {
-    return res.status(500).json(error);
+    console.error('An error occurred:', error); // Logga felet till konsolen
+    return res.status(500).json({ message: 'An error occurred', error: error.message }); 
   }
 });
 
